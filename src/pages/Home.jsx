fbe04e71fe27c5faf30bdaa5a598033f027d897d
Ravinodesousa <Ravinodesousa@gmail.com>
2024-06-27 19:11:04 +0100

    completed Home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Autocomplete from "../components/Autocomplete";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="center vh-100 ">
+      <div className="card">
+        <span className="card-title">Search for music....</span>
+        <Autocomplete />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
