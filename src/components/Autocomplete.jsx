792ad66a5bb892f0f6a16cc1645aed6fec2cda8a
Ravinodesousa <Ravinodesousa@gmail.com>
2024-06-28 13:56:43 +0100

    completed styling autocomplete input

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Autocomplete() {
-  return <div>Autocomplete</div>;
+  return (
+    <div>
+      <input
+        className="custom-input"
+        placeholder="Enter Author/Album/Song name....."
+      />
+    </div>
+  );
 }
 
 export default Autocomplete;
