dac0ffb8589389f7e25706a0bec0b425d0ef0b31
Ravinodesousa <Ravinodesousa@gmail.com>
2024-07-02 16:26:37 +0100

    wraped RecoilRoot in tests

--- src/tests/Autocomplete.test.js
+++ src/tests/Autocomplete.test.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Autocomplete from "../components/Autocomplete";
+import { RecoilRoot } from "recoil";
 
 describe("Autocomplete component", () => {
   it("renders without crashing", () => {
-    const { getByPlaceholderText } = render(<Autocomplete />);
+    const { getByPlaceholderText } = render(
+      <RecoilRoot>
+        <Autocomplete />
+      </RecoilRoot>
+    );
     expect(
       getByPlaceholderText("Enter Artist/Album/Song name.....")
     ).toBeInTheDocument();
